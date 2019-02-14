99ee5070170e7429be86663b0b113adc7ade492b
KRUNAL R PRAJAPATI <krunal@intellimedianetworks.net>
2019-02-14 11:27:40 +0530

    masonry ui

--- el/masonry.php
+++ el/masonry.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="../css/boot_custom.css">
+    <link rel="stylesheet" type="text/css" href="../css/global.css">
+    
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="../js/masonry.pkgd.min.js"></script>
     <script>
@@ -23,7 +26,9 @@
     .grid{position: relative;}
     .grid-item{
       width: 33.33%;
-      height: 200px;
+      height: calc(100vh/3);
+        /*width: calc(100vw/3);
+      height: calc(100vh/3);*/
       background-color: #f00;
       -webkit-transition: all 0.8s;
       -o-transition: all 0.8s;
